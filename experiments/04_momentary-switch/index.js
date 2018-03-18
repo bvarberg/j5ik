33f2b38cb32133f5ed22777b09598a81a185b3e0
Byron Varberg <byron@byronvarberg.com>
2018-03-18 00:36:58 -0500

    Handle hold event to control LED

--- experiments/04_momentary-switch/index.js
+++ experiments/04_momentary-switch/index.js
@@ -10,6 +10,7 @@ board.on('ready', () => {
   const button = new five.Button('a2');
 
   button.on('press', () => led.on());
-  button.on('release', () => led.off());
+  button.on('hold', () => led.blink(500));
+  button.on('release', () => led.stop().off());
 });
 
